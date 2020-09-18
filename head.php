12be593d7c742e4a896e590bb7747ff7c890ddb6
gxggxl <gxggxl@qq.com>
2020-09-18 21:54:49 +0800

    Update head.php

--- head.php
+++ head.php
@@ -21,7 +21,11 @@
 		      <script src="https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 		      <script src="https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
 		<![endif]-->
-	<style>
+	<style type="text/css">
+		html{
+			position: relative;
+			min-height: 100%;
+		}
 		body {
 			min-height: 2000px;
 			padding-top: 80px;
