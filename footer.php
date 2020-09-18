46a2cb6fabdc9ec4739b22968ada17ab76b6bc37
gxggxl <gxggxl@qq.com>
2020-09-18 21:53:31 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,2 +1,12 @@
+	
+	<footer class="navbar navbar-static-bottom">
+		<!-- navbar-fixed-bottom 这个类可以使其固定在网页底部，并且不随网页滚动 -->
+		<!-- navbar-static-bottom 如果需要随网页滚动，则使用这个类 -->
+		<div class="container text-center">
+			<span>Copyright &copy; 2020 桂ICP备0000000号</span>
+		</div>
+	</footer>
+	
 	</body>
+
 </html>
