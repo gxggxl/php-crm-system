32ac71a7b0fee1a5b14e72a15f4480e49bdb03e1
gxggxl <gxggxl@qq.com>
2020-09-19 23:31:36 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,21 +1,17 @@
 <?php
 # 数据库配置项
-$db_config = array(
-'database_type' => 'mysql',
-# *必填，默认数据库连接类型: MySQL, 其余类型 MariaDB, MSSQL, Sybase, PostgreSQL, Oracle
-'database_name' => 'rocboss_2_1',
+
 # *必填，数据库名称
-'server' => 'localhost',
+define("DBNAME", "test");
 # *必填，数据库主机名
-'username' => 'root',
+define("HOSTNAME", "localhost");
 # *必填，数据库用户名
-'password' => '123123',
+define("USERNAME", "root");
 # *必填，数据库密码
-'port' => 3306,
+define("PASSWORD", "");
 # 默认，数据库端口
-'charset' => 'utf8',
+define("PORT", "3306");
 # 默认，编码格式
-// 'option' => array( PDO::ATTR_CASE => PDO::CASE_NATURAL )
-# 默认，pdo选项，请确保pdo已经打开，配置 php.ini
-);
+define("CHARSET", "utf-8");
+
 ?>
