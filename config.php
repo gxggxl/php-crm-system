4b4350bce7312f38efbeff467cb9b62a27eb1da2
gxggxl <gxggxl@qq.com>
2020-09-21 20:44:37 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,4 @@
 <?php
-# 数据库配置项
-
 # *必填，数据库名称
 define("DBNAME", "test");
 # *必填，数据库主机名
@@ -14,12 +12,13 @@ define("PORT", "3306");
 # 默认，编码格式
 define("CHARSET", "utf8");
 
+# 数据库配置项
 $info = array(
-		'host' => 'localhost',
-		'port' => '3306 ',
-		'user' => 'root',
-		'password' => '',
-		'dbname' => 'test',
-		'charset' => 'utf8'
-	);
+'host' => 'localhost',
+'port' => '3306 ',
+'user' => 'root',
+'password' => '',
+'dbname' => 'test',
+'charset' => 'utf8'
+);
 ?>
