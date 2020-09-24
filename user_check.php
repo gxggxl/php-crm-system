4418111e503218a9298eab16867119add9d15d0c
gxggxl <gxggxl@qq.com>
2020-09-24 23:12:12 +0800

    Update user_check.php

--- user_check.php
+++ user_check.php
@@ -13,23 +13,22 @@ header('Content-type: application/json');
 // Bootstrapvalidator 用户后端实时检查
 $valid = true;//用户名可用
 // 查询用户是否存在
-if(isset($_POST['username'])){
-	$sql = "SELECT * FROM crm_users WHERE username='{$_POST['username']}'";
-	//查到数据返回false，bool取反；
-	$valid = !(bool) $db->read_one($sql);
-	// var_dump($valid);
-	// $valid = false;
-}elseif(isset($_POST['email'])){
-	$sql = "SELECT * FROM crm_users WHERE email='{$_POST['email']}'";
-	$valid = !(bool) $db->read_one($sql);
-}elseif(isset($_POST['phonenum'])){
-	$sql = "SELECT * FROM crm_users WHERE phonenum='{$_POST['phonenum']}'";
-	$valid = !(bool) $db->read_one($sql);
+if (isset($_POST['username'])) {
+    $sql = "SELECT * FROM crm_users WHERE username='{$_POST['username']}'";
+    //查到数据返回false，bool取反；
+    $valid = !(bool) $db->read_one($sql);
+// var_dump($valid);
+    // $valid = false;
+} elseif (isset($_POST['email'])) {
+    $sql = "SELECT * FROM crm_users WHERE email='{$_POST['email']}'";
+    $valid = !(bool) $db->read_one($sql);
+} elseif (isset($_POST['phonenum'])) {
+    $sql = "SELECT * FROM crm_users WHERE phonenum='{$_POST['phonenum']}'";
+    $valid = !(bool) $db->read_one($sql);
 }
 
 echo json_encode(
-	array(
-		'valid' => $valid,
-	)
+    array(
+        'valid' => $valid,
+    )
 );
-?>
