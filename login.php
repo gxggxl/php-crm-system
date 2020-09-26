e29cb47cc0b5f2f3728fdb814f6135602a32feb3
gxggxl <gxggxl@qq.com>
2020-09-26 12:54:29 +0800

    Update login.php

--- login.php
+++ login.php
@@ -7,41 +7,41 @@
  * @FilePath     : /php-crm-system/login.php
  */
 ?>
-		<div class="container">
-			<div class="row login">
-				<div class="col-md-6" style="border-right:1px solid #ddd;">
-					<form action="login_check.php" method="post">
-					<div class="well col-md-12">
-						<h3 class="login-title">用户登录</h3>
-						<div class="input-group input-group-md">
-							<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-user" aria-hidden="true"></i></span>
-							<input type="text" class="form-control" name="username" placeholder="用户名" aria-describedby="sizing-addon1">
-						</div>
-						<div class="input-group input-group-md">
-							<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-lock"></i></span>
-							<input type="password" class="form-control" name="password" placeholder="密码" aria-describedby="sizing-addon1">
-						</div>
-						<div class="input-group input-group-md">
-							<input type="checkbox" class="row" name="remember" value="1"><span>记住我</span>
-						</div>
-						<!-- <div class="well well-sm" style="text-align:center;">
-							<input type="radio" name="kind" value="tea"> 管理员
-							<input type="radio" name="kind" value="stu"> 客户
-						</div> -->
-						<button type="submit" class="btn btn-success btn-block">登录</button>
+	<div class="container">
+		<div class="row login">
+			<div class="col-md-6" style="border-right:1px solid #ddd;">
+				<form action="login_check.php" method="post">
+				<div class="well col-md-12">
+					<h3 class="login-title">用户登录</h3>
+					<div class="input-group input-group-md">
+						<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-user" aria-hidden="true"></i></span>
+						<input type="text" class="form-control" name="username" placeholder="用户名" aria-describedby="sizing-addon1">
 					</div>
-					</form>
+					<div class="input-group input-group-md">
+						<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-lock"></i></span>
+						<input type="password" class="form-control" name="password" placeholder="密码" aria-describedby="sizing-addon1">
+					</div>
+					<div class="input-group input-group-md">
+						<input type="checkbox" class="row" name="remember" value="1"><span>记住我</span>
+					</div>
+					<!-- <div class="well well-sm" style="text-align:center;">
+						<input type="radio" name="kind" value="tea"> 管理员
+						<input type="radio" name="kind" value="stu"> 客户
+					</div> -->
+					<button type="submit" class="btn btn-success btn-block">登录</button>
 				</div>
-				<div class="col-md-6">
-					<h3>
-						欢迎使用客户管理系统
-					</h3>
-					<ul class="info-a">
-						<li>管理员使用<em>admin</em>登录，初始密码为<em>123456</em>，登录后请及时修改密码</li>
-						<li>客户请使用<em>user</em>登录，初始密码为<em>123456</em>，登录后请及时修改密码</li>
-					</ul>
-				</div>
+				</form>
+			</div>
+			<div class="col-md-6 well">
+				<h3>
+					欢迎使用客户管理系统
+				</h3>
+				<ul class="info-a">
+					<li>管理员使用<em>admin</em>登录，初始密码为<em>123456</em>，登录后请及时修改密码</li>
+					<li>客户请使用<em>user</em>登录，初始密码为<em>123456</em>，登录后请及时修改密码</li>
+				</ul>
 			</div>
 		</div>
+	</div>
 		
 <?php include 'footer.php'; ?>
