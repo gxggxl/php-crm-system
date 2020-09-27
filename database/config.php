802ff1f9467b9416a238292dd530da76540bd913
gxggxl <gxggxl@qq.com>
2020-09-27 22:46:43 +0800

    Update config.php

--- database/config.php
+++ database/config.php
@@ -7,26 +7,14 @@
  * @FilePath     : /php-crm-system/database/config.php
  */
 
-// # *必填，数据库名称
-// define("DBNAME", "test");
-// # *必填，数据库主机名
-// define("HOSTNAME", "localhost");
-// # *必填，数据库用户名
-// define("USERNAME", "root");
-// # *必填，数据库密码
-// define("PASSWORD", "");
-// # 默认，数据库端口
-// define("PORT", "3306");
-// # 默认，编码格式
-// define("CHARSET", "utf8");
-
 # 数据库配置项
 $dbinfo = array(
-    'host' => 'localhost',
-    'port' => '3306 ',
-    'user' => 'test',
-    'password' => '123456',
-    'dbname' => 'test',
-    'charset' => 'utf8'
+	'host'     => 'localhost',
+	'port'     => '3306 ',
+	'user'     => 'test',
+	'password' => '123456',
+	'dbname'   => 'test',
+	'charset'  => 'utf8',
 );
+
 ?>
