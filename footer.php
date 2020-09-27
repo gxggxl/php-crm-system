a9bcef4126d1350d44c100c2690923c1333fb09f
gxggxl <gxggxl@qq.com>
2020-09-27 22:16:01 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,22 +1,22 @@
 
-	<footer class="navbar navbar-static-bottom footer">
-		<!-- navbar-fixed-bottom 这个类可以使其固定在网页底部，并且不随网页滚动 -->
-		<!-- navbar-static-bottom 如果需要随网页滚动，则使用这个类 -->
-		<div class="container">
+<footer class="navbar navbar-static-bottom footer">
+	<!-- navbar-fixed-bottom 这个类可以使其固定在网页底部，并且不随网页滚动 -->
+	<!-- navbar-static-bottom 如果需要随网页滚动，则使用这个类 -->
+	<div class="container">
+		<div>
 			<div>
-				<div>
-					<div class="text-center">
-						<span>Copyright &copy; 2020
-							<a href="http://beian.miit.gov.cn/" target="_blank">桂ICP备18002684号-3</a>&nbsp;|
-							<a href="https://gxusb.com" target="_blank">Blog</a>&nbsp;|
-							<a href="https://github.com/gxggxl/php-crm-system" target="_blank">项目开源地址</a>
-						</span>
-					</div>
+				<div class="text-center">
+					<span>Copyright &copy; 2020
+						<a href="http://beian.miit.gov.cn/" target="_blank">桂ICP备18002684号-3</a>&nbsp;|
+						<a href="https://gxusb.com" target="_blank">Blog</a>&nbsp;|
+						<a href="https://github.com/gxggxl/php-crm-system" target="_blank">项目开源地址</a>
+					</span>
 				</div>
 			</div>
 		</div>
-	</footer>
+	</div>
+</footer>
 
-	</body>
+</body>
 
 </html>
