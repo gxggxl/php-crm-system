a31d24da664727d86625e440af6262f1d245a075
gxggxl <gxggxl@qq.com>
2020-09-27 20:17:29 +0800

    分页类测试

---
+++ page/demo.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * @Author       : gxggxl
+ * @E-mail       : gxggxl@qq.com
+ * @Date         : 2020-09-27 14:41:08
+ * @LastEditTime : 2020-09-27 20:11:29
+ * @FilePath     : /php-crm-system/page/demo.php
+ */
+
+header("Content-Type:text/html;charset=utf-8");
+include "page.class.php";
+
+$link=mysqli_connect("127.0.0.1","test","123456","test");
+
+$result=mysqli_query($link,"select * from crm_users");
+
+$total=mysqli_num_rows($result);
+
+$num=10;
+
+$page=new Page($total,$num);
+
+$sql="select * from crm_users {$page->limit}";
+
+echo '<table border="1" align="center" cellspacing="" cellpadding="" width="900">';
+echo '<caption><h1>Users</h1></caption>';
+	while($row=mysqli_fetch_assoc($result)){
+		echo '<tr>';
+		echo '<td>'.$row["uid"].'</td>';
+		echo '<td>'.$row["username"].'</td>';
+		echo '<td>'.$row["sex"].'</td>';
+		echo '<td>'.$row["email"].'</td>';
+		echo '<td>'.$row["phonenum"].'</td>';
+		echo '<td>'.$row["createtime"].'</td>';
+		echo '<tr>';
+	}
+	// <tr><th>Header</th></tr>
+echo '<tr><td colspan="6" align="right">'.$page->fpage().'</td></tr>';
+echo '</table>';
+
+?>
+
