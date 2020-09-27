33d7cc25d5efe36b268239425ba10baa3f6b2ac4
gxggxl <gxggxl@qq.com>
2020-09-27 23:30:56 +0800

    Update demo.php

--- page/demo.php
+++ page/demo.php
@@ -10,33 +10,33 @@
 header("Content-Type:text/html;charset=utf-8");
 include "page.class.php";
 
-$link=mysqli_connect("127.0.0.1","test","123456","test");
+$link = mysqli_connect("127.0.0.1", "test", "123456", "test");
 
-$result=mysqli_query($link,"select * from crm_users");
+$result = mysqli_query($link, "select * from crm_users");
 
-$total=mysqli_num_rows($result);
+$total = mysqli_num_rows($result);
 
-$num=5;
+$num = 5;
 
-$page=new Page($total,$num);
+$page = new Page($total, $num);
 
-$sql="select * from crm_users {$page->limit}";
+$sql = "select * from crm_users {$page->limit}";
 
-$result = mysqli_query($link,$sql);
+$result = mysqli_query($link, $sql);
 
 echo '<table border="1" align="center" cellspacing="" cellpadding="" width="900">';
 echo '<caption><h1>Users</h1></caption>';
 echo '<tr><th>uid</th><th>username</th><th>sex</th><th>email</th><th>phonenum</th><th>createtime</th></tr>';
-	while($row=mysqli_fetch_assoc($result)){
-		echo '<tr>';
-		echo '<td>'.$row["uid"].'</td>';
-		echo '<td>'.$row["username"].'</td>';
-		echo '<td>'.$row["sex"].'</td>';
-		echo '<td>'.$row["email"].'</td>';
-		echo '<td>'.$row["phonenum"].'</td>';
-		echo '<td>'.$row["createtime"].'</td>';
-		echo '<tr>';
-	}
+while ($row = mysqli_fetch_assoc($result)) {
+	echo '<tr>';
+	echo '<td>'.$row["uid"].'</td>';
+	echo '<td>'.$row["username"].'</td>';
+	echo '<td>'.$row["sex"].'</td>';
+	echo '<td>'.$row["email"].'</td>';
+	echo '<td>'.$row["phonenum"].'</td>';
+	echo '<td>'.$row["createtime"].'</td>';
+	echo '<tr>';
+}
 echo '<tr><td colspan="6" align="right">'.$page->fpage().'</td></tr>';
 echo '</table>';
 
