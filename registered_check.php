844dc62db99bcfacc084da3000b44ee878e60849
gxggxl <gxggxl@qq.com>
2020-09-27 23:28:23 +0800

    Update registered_check.php

--- registered_check.php
+++ registered_check.php
@@ -3,12 +3,12 @@
 include "database/conn.php";
 header('content-type:text/html;charset=utf-8');
 
-$username = $_POST["username"];
-$email = $_POST["email"];
-$sex = $_POST["sex"];
-$phonenum = $_POST["phonenum"];
-$pwd = $_POST["pwd"];
-$pwd1 = $_POST["pwd1"];
+$username   = $_POST["username"];
+$email      = $_POST["email"];
+$sex        = $_POST["sex"];
+$phonenum   = $_POST["phonenum"];
+$pwd        = $_POST["pwd"];
+$pwd1       = $_POST["pwd1"];
 $createtime = time();
 // Y ：年（四位数）大写
 // m : 月（两位数，首位不足补0） 小写
@@ -24,33 +24,33 @@ $createtime = time();
 $password = md5($pwd);
 
 if (isset($_POST['username'])) {
-    $sql = "SELECT * FROM crm_users WHERE username='{$_POST['username']}'";
-    $res = (bool) $db->read_one($sql);
-    if ($res) {
-        exit("用户名"."{$_POST['username']}"."已注册");
-    }
+	$sql = "SELECT * FROM crm_users WHERE username='{$_POST['username']}'";
+	$res = (bool) $db->read_one($sql);
+	if ($res) {
+		exit("用户名"."{$_POST['username']}" ."已注册");
+	}
 }
 if (isset($_POST['email'])) {
-    $sql = "SELECT * FROM crm_users WHERE email='{$_POST['email']}'";
-    $res = (bool) $db->read_one($sql);
-    if ($res) {
-        exit("{$_POST['email']}"."邮箱已注册");
-    }
+	$sql = "SELECT * FROM crm_users WHERE email='{$_POST['email']}'";
+	$res = (bool) $db->read_one($sql);
+	if ($res) {
+		exit("{$_POST['email']}" ."邮箱已注册");
+	}
 }
 if (isset($_POST['phonenum'])) {
-    $sql = "SELECT * FROM crm_users WHERE phonenum='{$_POST['phonenum']}'";
-    $res = (bool) $db->read_one($sql);
-    if ($res) {
-        exit("手机号码:"."{$_POST['phonenum']}"."已注册");
-    }
+	$sql = "SELECT * FROM crm_users WHERE phonenum='{$_POST['phonenum']}'";
+	$res = (bool) $db->read_one($sql);
+	if ($res) {
+		exit("手机号码:"."{$_POST['phonenum']}" ."已注册");
+	}
 }
 
-if ((isset($pwd)==isset($pwd1)) && isset($username)) {
-    $sql = "INSERT INTO `crm_users`(`username`, `password`, `email`, `sex`, `phonenum`, `createtime`) VALUES ('{$username}','{$password}','{$email}','{$sex}','{$phonenum}','{$createtime}')";
-    $res = $db->write($sql);
-    $uid=$db->insert_id();
-    echo "注册成功，你的ID为 ".$uid."用户名为：".$username;
-// var_dump($res);//int(1)
+if ((isset($pwd) == isset($pwd1)) && isset($username)) {
+	$sql = "INSERT INTO `crm_users`(`username`, `password`, `email`, `sex`, `phonenum`, `createtime`) VALUES ('{$username}','{$password}','{$email}','{$sex}','{$phonenum}','{$createtime}')";
+	$res = $db->write($sql);
+	$uid = $db->insert_id();
+	echo "注册成功，你的ID为 ".$uid."用户名为：".$username;
+	// var_dump($res);//int(1)
 } else {
-    echo "注册失败，请检查表单是否填写正确。";
+	echo "注册失败，请检查表单是否填写正确。";
 }
