ce4f7d8453cdefd25202d0f4900b19b91ea9fe10
gxggxl <gxggxl@qq.com>
2020-09-27 23:51:10 +0800

    Update logout.php

--- tool/logout.php
+++ tool/logout.php
@@ -1,5 +1,9 @@
 <?php
-// 退出登录
+
+/**
+ * [logout 退出登录]
+ * @return [type] [清除COOKIE]
+ */
 function logout() {
 	unset($_SESSION['user_info']);
 	if (!empty($_COOKIE['username']) || !empty($_COOKIE['password'])) {
