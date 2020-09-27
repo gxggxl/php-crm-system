3b4faa3a8c6f13996217ae877842fa1c3ca34fd4
gxggxl <gxggxl@qq.com>
2020-09-27 23:53:01 +0800

    Update cookie_check.php

--- cookie_check.php
+++ cookie_check.php
@@ -1,14 +1,18 @@
 <?php
-    
+
 //一般发在头文件进行登陆判断
-function cookieCheck(){
+/**
+ * [cookieCheck 登陆判断]
+ * @return [bol] [description]
+ */
+function cookieCheck() {
 	if (!isset($_COOKIE['username'])) {
-	//window写法(标准)
-	// echo "<script>alert('当前用户未登录！');window.location='login.php'</script>";
-	    //header写法
-	    echo "<script>alert('当前用户未登录！')</script>";
-	    header("refresh:1;url=login.php");
-	    //refresh 多少秒后唤醒
-	    exit;
+		//window写法(标准)
+		// echo "<script>alert('当前用户未登录！');window.location='login.php'</script>";
+		//header写法
+		echo "<script>alert('当前用户未登录！')</script>";
+		header("refresh:1;url=login.php");
+		//refresh 多少秒后唤醒
+		exit;
 	}
 }
