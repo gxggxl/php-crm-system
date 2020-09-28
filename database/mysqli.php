ad6514faf2825747d02987eea91b6eda8316d2c7
gxggxl <gxggxl@qq.com>
2020-09-28 11:34:47 +0800

    Update mysqli.php

--- database/mysqli.php
+++ database/mysqli.php
@@ -8,7 +8,7 @@
  */
 
 # 数据库操作类
-class Sql{
+class Sql {
 	# 定义属性:保存数据库初始化的信息
 	# 主机
 	private $host;
@@ -24,62 +24,42 @@ class Sql{
 	private $charset;
 	# 数据库连接字符串
 	private $link;
-	# 错误编码
-	public $errno;
-	# 错误信息
-	public $error;
 	# 列数
 	public $columns = 0;
 	# 行数
 	public $rows;
 	# 二维数组
 	public $list;
+	# 错误编码
+	public $errno;
+	# 错误信息
+	public $error;
 
 	# 实现数据的初始化：灵活性(允许外部修改)和通用性(给定默认值)
-	public function __construct(array $arr = []){
+	public function __construct(array $arr = []) {
 		# 完成初始化
-		$this->host = $arr['host'] ?? 'localhost';
-		$this->port = $arr['port'] ?? '3306';
-		$this->user = $arr['user'] ?? 'root';
-		$this->password = $arr['password'] ?? 'root';
-		$this->dbname = $arr['dbname'] ?? 'test';
-		$this->charset = $arr['charset'] ?? 'utf8';
+		$this->host     = $arr['host']??'localhost';
+		$this->port     = $arr['port']??'3306';
+		$this->user     = $arr['user']??'root';
+		$this->password = $arr['password']??'root';
+		$this->dbname   = $arr['dbname']??'test';
+		$this->charset  = $arr['charset']??'utf8';
 
 		# 实现初始化数据库操作
-		# 为了中断执行
-		if(!$this->connect()) return;
+		if (!$this->connect()) {
+			# 为了中断执行
+			return;
+		}
+
 		# 设置字符集
 		$this->charset();
-		//
-		//                       _oo0oo_
-		//                      o8888888o
-		//                      88" . "88
-		//                      (| -_- |)
-		//                      0\  =  /0
-		//                    ___/`---'\___
-		//                  .' \\|     |// '.
-		//                 / \\|||  :  |||// \
-		//                / _||||| -:- |||||- \
-		//               |   | \\\  - /// |   |
-		//               | \_|  ''\---/''  |_/ |
-		//               \  .-\__  '-'  ___/-. /
-		//             ___'. .'  /--.--\  `. .'___
-		//          ."" '<  `.___\_<|>_/___.' >' "".
-		//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-		//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-		//     =====`-.____`.___ \_____/___.-`___.-'=====
-		//                       `=---='
-		//
-		//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		//
-		//           佛祖保佑       永不宕机     永无BUG
 	}
 
 	# 连接认证
-	private function connect(){
-		$this->link = @mysqli_connect($this->host,$this->user,$this->password,$this->dbname,$this->port);
+	private function connect() {
+		$this->link = @mysqli_connect($this->host, $this->user, $this->password, $this->dbname, $this->port);
 		# 加工结果
-		if(!$this->link){
+		if (!$this->link) {
 			# 记录错误信息，返回false
 			echo "数据库连接失败<br>";
 			echo "错误编码:".mysqli_connect_errno($this->link)."<br>";
@@ -96,12 +76,12 @@ class Sql{
 	 * [charset 设置字符集]
 	 * @return [boolean]     [返回结果]
 	 */
-	private function charset(){
+	private function charset() {
 		# 利用实现mysqli设置字符集
-		$res = @mysqli_set_charset($this->link,$this->charset);
+		$res = @mysqli_set_charset($this->link, $this->charset);
 		# mysqli_query($this->link,"set names {$this->charset}");
 		# 判定错误
-		if(!$res){
+		if (!$res) {
 			echo "设置字符集失败<br>";
 			echo "错误编码:".$this->errno = mysqli_errno($this->link)."<br>";
 			echo "错误信息:".$this->error = mysqli_error($this->link)."<br>";
@@ -118,10 +98,10 @@ class Sql{
 	 * @param  [string] $sql [sql语句]
 	 * @return [boolean]     [返回结果]
 	 */
-	private function check($sql){
-		$res = @mysqli_query($this->link,$sql);
+	private function check($sql) {
+		$res = @mysqli_query($this->link, $sql);
 		# 判定错误
-		if(!$res){
+		if (!$res) {
 			$this->errno = mysqli_errno($this->link);
 			$this->error = mysqli_error($this->link);
 			return false;
@@ -131,18 +111,18 @@ class Sql{
 	}
 
 	# SQL写操作
-	public function write($sql){
+	public function write($sql) {
 		# 调用SQL检查方法检查和执行
 		$res = $this->check($sql);
 		# 根据结果判定:如果$res为true, 说明执行成功，应该获取受影响的行数，如果为false就返回false
-		return $res ? mysqli_affected_rows($this->link) : false;
+		return $res?mysqli_affected_rows($this->link):false;
 	}
 
 	/**
 	 * [insert_id 获取自增长ID的方法]
 	 * @return [int] [返回增长ID]
 	 */
-	public function insert_id(){
+	public function insert_id() {
 		return mysqli_insert_id($this->link);
 	}
 
@@ -151,13 +131,13 @@ class Sql{
 	 * @param  [srting] $sql [数据库语句]
 	 * @return [Array]   	 [一维数组]
 	 */
-	public function read_one($sql){
+	public function read_one($sql) {
 		# 执行检查
 		$res = $this->check($sql);
 		# 判定结果
-		if($res){
-		# 有结果
-		$this->columns = @mysqli_field_count($this->link);
+		if ($res) {
+			# 有结果
+			$this->columns = @mysqli_field_count($this->link);
 			return mysqli_fetch_assoc($res);
 		}
 		# 没有结果
@@ -168,17 +148,19 @@ class Sql{
 	 * @param  [srting] $sql [数据库语句]
 	 * @return [Array]     	 [二维数组]
 	 */
-	public function read_all($sql){
+	public function read_all($sql) {
 		# 执行检查
 		$res = $this->check($sql);
 		# 错误检查
-		if( !$res) return false;
+		if (!$res) {return false;
+		}
+
 		# 结果正确 记录结果数量
-		$this->rows = @mysqli_num_rows($res);
+		$this->rows    = @mysqli_num_rows($res);
 		$this->columns = @mysqli_field_count($this->link);
 		# 根据需求解析数据 循环取出所有记录:形成二维数组
-		$list = [];
-		while($row = mysqli_fetch_assoc($res)) $list[] = $row;
+		$list                                          = [];
+		while ($row = mysqli_fetch_assoc($res))$list[] = $row;
 		# 返回结果
 		return $list;
 	}
