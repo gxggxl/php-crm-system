5ab3939a32316f36c32701e29c8b3e42520fc147
gxggxl <gxggxl@qq.com>
2020-11-02 00:43:08 +0800

    Update test.php

--- test.php
+++ test.php
@@ -6,23 +6,22 @@
  * @FilePath : test.php
  */
 // 引入数据库操作文件
-include_once dirname(__FILE__) . "/database/conn.php";
+include_once __DIR__ . "/database/conn.php";
 
 $pass = md5(md5("123456") . "gx");
 // string(32) "e0ba03690d506d5a6c03ba1228ea5ca5"
-var_dump($pass);
+//var_dump($pass);
 
 $pas = md5("123456");
 // string(32) "e10adc3949ba59abbe56e057f20f883e"
-var_dump($pas);
+//var_dump($pas);
 
 //获取用户IP地址
-print_r('<pre>用户IP地址：' . $_SERVER['SERVER_ADDR'] . '</pre>');
+echo '<pre>用户IP地址：' . $_SERVER['SERVER_ADDR'] . '</pre>';
 //获取用户UA
-print_r('<pre>用户UA：' . $_SERVER['HTTP_USER_AGENT'] . '</pre>');
+echo '<pre>用户UA：' . $_SERVER['HTTP_USER_AGENT'] . '</pre>';
 //var_dump($_SERVER);
-
-var_dump(__DIR__);//文件路径
+echo '脚本文件路径：'.__DIR__.'<br>';
 
 // Y ：年（四位数）大写
 // m : 月（两位数，首位不足补0） 小写
@@ -33,8 +32,8 @@ var_dump(__DIR__);//文件路径
 // s ：带有首位零的秒（00 -59）
 // a：小写的午前和午后（am 或 pm）
 
-//$time = date('Y-m-d H:i:s', time());
-//// int(1600849580)
+echo '服务器时间：'.date('Y-m-d H:i:s',time()).'<br>';
+// int(1600849580)
 //var_dump($time);
 
 //$str = "  Hello World!";
