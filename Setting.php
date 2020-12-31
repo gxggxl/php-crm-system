d3fbe761121b42f4d183f181d15f6e7a90d84956
gxggxl <gxggxl@qq.com>
2020-12-31 10:31:02 +0800

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * @Author       : gxggxl
+ * @E-mail       : gxggxl@qq.com
+ * @Date         : 2020-10-17 18:25:57
+ * @LastEditTime : 2020-11-07 21:29:54
+ * @FilePath     : /php-crm-system/Setting.php
+ */
 /**
  * @author   ：gxggxl
  * @BlogURL  : https://gxusb.com
@@ -6,5 +13,4 @@
  * @FilePath : Setting.php
  */
 function show(){
-
 }
