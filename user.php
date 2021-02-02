db0a09a5498a12fb5f888906a8cc7740cee2e1b7
gxggxl <gxggxl@qq.com>
2021-02-02 19:19:19 +0800

    Update user.php

--- user.php
+++ user.php
@@ -5,7 +5,7 @@ include_once "page/page.class.php";
 
 // 验证登录状态
 if (empty($_COOKIE['username']) && empty($_COOKIE['password'])) {
-    exit("<script>alert('你没有登录！请登录。');window.location='login.php';</script>");
+	exit("<script>alert('你没有登录！请登录。');window.location='login.php';</script>");
 }
 ?>
 
@@ -30,32 +30,31 @@ if (empty($_COOKIE['username']) && empty($_COOKIE['password'])) {
             </div>
             <div class="col-md-8">
                 <div class="panel panel-default table-responsive">
-                    <?php
-                    $sql2 = "select * from crm_users";
-                    $db->readAll($sql2);
-                    //获取行数
-                    $total = $db->rows;
+					<?php
+					$sql2 = "select * from crm_users";
+					$db->readAll($sql2);
+					//获取行数
+					$total = $db->rows;
 
-                    $pageSize = $_GET['size'] ?? 10;
-                    // var_dump($total);
+					$pageSize = $_GET['size'] ?? 10;
+					// var_dump($total);
 
-                    $page = new Page($total, $pageSize);
-                    $limit = $page->limit();
-                    $sql1 = "select * from crm_users limit {$limit}";
-                    $rows = $db->readAll($sql1);
-                    // var_dump($limit,$page ,$sql1);
+					$page = new Page($total, $pageSize);
+					$limit = $page->limit();
+					$sql1 = "select * from crm_users limit {$limit}";
+					$rows = $db->readAll($sql1);
+					// var_dump($limit,$page ,$sql1);
 
-                    /**
-                     * 检测数字奇偶
-                     * @return BOOL
-                     * @var $num
-                     */
-                    function checkNum($num)
-                    {
-                        return (bool)($num%2);
-                    }
+					/**
+					 * 检测数字奇偶
+					 * @return BOOL
+					 * @var $num
+					 */
+					function checkNum($num) {
+						return (bool)($num % 2);
+					}
 
-                    echo '<table class="table table-hover table-condensed table-responsive text-nowrap">
+					echo '<table class="table table-hover table-condensed table-responsive text-nowrap">
                           <caption><h1 class="text-center">Users</h1></caption>
                           <thead><tr>
 		                  <th scope="row">用户ID</th>
@@ -67,45 +66,45 @@ if (empty($_COOKIE['username']) && empty($_COOKIE['password'])) {
 		                  <th scope="row">注册IP地址</th>
 		                  </tr></thead>
                           <tbody>';
-                    for ($i = 0, $iMax = count($rows); $i < $iMax; $i++) {
-                        //表格隔行加了点颜色
-                        if (checkNum($i) === false) {
-                            echo '<tr class="success">';
-                        } else {
-                            echo '<tr>';
-                        }
-                        echo '<td>' . $rows[$i]["uid"] . '</td>';
-                        echo '<td>' . $rows[$i]["username"] . '</td>';
-                        echo '<td>' . $rows[$i]["sex"] . '</td>';
-                        echo '<td>' . $rows[$i]["email"] . '</td>';
-                        echo '<td>' . $rows[$i]["phone_num"] . '</td>';
-                        echo '<td>' . date('Y年m月d日 H:i:s', $rows[$i]["create_time"]) . '</td>';
-                        echo '<td>' . $rows[$i]["ip"] . '</td>';
-                        echo '</tr>';
-                    }
-                    echo '<tr><td style="text-align: right" colspan="7">' . $page->showPage() . '</td></tr>';
-                    echo '</tbody>';
-                    echo '</table>';
+					for ($i = 0, $iMax = count($rows); $i < $iMax; $i++) {
+						//表格隔行加了点颜色
+						if (checkNum($i) === false) {
+							echo '<tr class="success">';
+						} else {
+							echo '<tr>';
+						}
+						echo '<td>' . $rows[$i]["uid"] . '</td>';
+						echo '<td>' . $rows[$i]["username"] . '</td>';
+						echo '<td>' . $rows[$i]["sex"] . '</td>';
+						echo '<td>' . $rows[$i]["email"] . '</td>';
+						echo '<td>' . $rows[$i]["phone_num"] . '</td>';
+						echo '<td>' . date('Y年m月d日 H:i:s', $rows[$i]["create_time"]) . '</td>';
+						echo '<td>' . $rows[$i]["ip"] . '</td>';
+						echo '</tr>';
+					}
+					echo '<tr><td style="text-align: right" colspan="7">' . $page->showPage() . '</td></tr>';
+					echo '</tbody>
+                          </table>';
 
-                    // var_dump(count($rows,0));
-                    // echo "<pre>";
-                    // print_r($rows);
-                    // echo "</pre>";
-                    ?>
+					// var_dump(count($rows,0));
+					// echo "<pre>";
+					// print_r($rows);
+					// echo "</pre>";
+					?>
                 </div>
             </div>
 
             <div class="col-md-2">
                 <div class="well well-sm">
-                    <?php
-                    $sql = "SELECT * FROM crm_users WHERE username='{$_COOKIE['username']}'";
-                    $res = $db->readOne($sql);
-                    echo "uid:" . $res['uid'] . "<br>";
-                    echo "用户名:" . $res['username'] . "<br>";
-                    echo "性别:" . $res['sex'] . "<br>";
-                    echo "邮箱:" . $res['email'] . "<br>";
-                    echo "手机:" . $res['phone_num'] . "<br>";
-                    ?>
+					<?php
+					$sql = "SELECT * FROM crm_users WHERE username='{$_COOKIE['username']}'";
+					$res = $db->readOne($sql);
+					echo "uid:" . $res['uid'] . "<br>";
+					echo "用户名:" . $res['username'] . "<br>";
+					echo "性别:" . $res['sex'] . "<br>";
+					echo "邮箱:" . $res['email'] . "<br>";
+					echo "手机:" . $res['phone_num'] . "<br>";
+					?>
                 </div>
             </div>
         </div>
