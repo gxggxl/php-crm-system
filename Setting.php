50e383391479554719058adca974995f2ff31c13
gxggxl <gxggxl@qq.com>
2021-04-09 12:58:12 +0800

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -1,11 +1,4 @@
 <?php
-/*
- * @Author       : gxggxl
- * @E-mail       : gxggxl@qq.com
- * @Date         : 2020-10-17 18:25:57
- * @LastEditTime : 2020-11-07 21:29:54
- * @FilePath     : /php-crm-system/Setting.php
- */
 /**
  * @author   ：gxggxl
  * @BlogURL  : https://gxusb.com
